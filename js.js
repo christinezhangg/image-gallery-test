8feaee0238f5c4b25ae1103b467e419daf714308
christinezhangg <54513295+christinezhangg@users.noreply.github.com>
2022-11-04 02:34:29 -0500

    figuring out arrows

--- js.js
+++ js.js
@@ -1,1 +1,15 @@
+var images = document.getElementsByClassName("images");
+var imageId = 0;
 
+for (i = 0; i < images.length; ++i){
+    images[i].addEventListener("click", bigimage);
+}
+function bigimage() {
+    if (i){
+        this.style.width = '200%';
+        this.style.height = 'auto';
+    } else {
+        blur();
+    }
+    
+}
